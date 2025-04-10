6add6db37777166f19d0d0c2c541c21fbf22944d
jaque08 <146119995+jaque08@users.noreply.github.com>
2025-04-10 15:44:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,10 +16,12 @@ for (let i = 0; i < botoes.length; i++) {
   }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2025-11-11T00:00:00");
+const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo) {
   let tempoAtual = new Date();
@@ -45,6 +47,20 @@ horas %= 24;
     " segundos"
   );
 }
+
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
+
 
 
 
