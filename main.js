f94b5600d7e79c373380079535042c996343a27c
jaque08 <146119995+jaque08@users.noreply.github.com>
2025-04-10 15:50:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-11-11T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-07-10T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-04-10T00:00:00");
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
