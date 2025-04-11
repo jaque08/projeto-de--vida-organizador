be197efebd6237252d40620c064639f250f27394
jaque08 <146119995+jaque08@users.noreply.github.com>
2025-04-10 21:08:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -31,30 +31,33 @@ function calculaTempo(tempoObjetivo) {
   let horas = Math.floor(minutos / 60);
   let dias = Math.floor(horas / 24);
 
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-
-    if (tempoFinal > 0) {
-        return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos");
-    } else {
-        return "Prazo Finalizado";
-    }
-
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  if (tempoFinal > 0) {
+    return [dias, horas, minutos, segundos];
+  } else {
+    return [0, 0, 0, 0];
+  }
 }
 
 function atualizaCronometro() {
-    for (let i = 0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
-    }
+  document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+  document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
+  document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+  document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
+  for (let i = 0; i < contadores.length; i++) {
+    //contadores[i].textContent = calculaTempo(tempos[i]);
+  }
 }
+
 
 function comecaCronometro() {
     atualizaCronometro();
     setInterval(atualizaCronometro, 1000);
 }
 
-//comecaCronometro();
+comecaCronometro();
 
 
 
